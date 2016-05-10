6917041b8695e1d5c0a9ce5c7597f6bc5f5466ba
Abdus Salam <salam4268@gmail.com>
2016-05-11 04:19:44 +1200

    Create Celebrating Victory (বিজয় উৎযাপন).c

---
+++ Celebrating Victory (বিজয় উৎযাপন).c
@@ -0,0 +1,16 @@
+/*
+*Date: 10/05/2016
+*Author: Abdus Salam
+*Contact: salam4268@gmail.com
+*Source: http://uva.onlinejudge.org
+*Problem No: 01
+*Name: Celebrating Victory (বিজয় উৎযাপন)
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    printf("Yaayy! Bangladesh Won!!\n");
+    return 0;
+}
